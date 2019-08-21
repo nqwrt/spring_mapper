9d0323c61a9951a297f2987f2a6779edc35b8b20
nqwrt <nqwrt@DESKTOP-46ICTHV>
2019-08-21 21:21:41 +0900

    테스트

--- src/test/java/edu/bit/mapper/BoardMapperTest.java
+++ src/test/java/edu/bit/mapper/BoardMapperTest.java
@@ -9,22 +9,21 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import edu.bit.mapper.SampleMapper;
+import edu.bit.ex.vo.BoardVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
-public class SampleMapperTest {
+public class BoardMapperTest {
 
-	@Inject
-	private SampleMapper sampleMapper;	
+	@Inject 
+	private BoardMapper boardMapper;
 	
-
+	
 	@Test
-	public void testGetTime() {
-		String  now = sampleMapper.getTime();
-		System.out.println("now = " + now);
+	public void testSelectBoardList() {
+		BoardVO boardVO = boardMapper.selectBoardOne("522");
+		System.out.println(boardVO.getbHit());
 	}
-	
-	
+
 
 }
